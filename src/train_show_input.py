9e156b8498add9115452d3d3a026484f9df96693
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-01 14:20:02 +0900

    show the initial input graph in *-pred.png

--- src/train_show_input.py
+++ src/train_show_input.py
@@ -145,6 +145,7 @@ def SaveWall(sample_batched, pred_label_wall, gt_wall, it):
         q0 = (int((p0[i, 0] - xMin) / resolution), int((p0[i, 1] - yMin) / resolution))
         q1 = (int((p1[i, 0] - xMin) / resolution), int((p1[i, 1] - yMin) / resolution))
         cv2.line(img_line, q0, q1, (255, 255, 255), 1)
+        cv2.line(img_line1, q0, q1, (255, 255, 255), 1)
         if gt_wall[i] == 1:
             cv2.line(img_line, q0, q1, (0, 0, 255), 5)
         if pred_label_wall[i] == 1:
